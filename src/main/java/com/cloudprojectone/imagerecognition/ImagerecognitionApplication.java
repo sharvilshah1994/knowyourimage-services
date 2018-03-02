ff039b3c468c66a957cfb3c09059fc32145e1a0e
Sharvil Shah <sharvil@sharvil.local>
2018-03-01 17:35:28 -0700

    Initial commit

---
+++ src/main/java/com/cloudprojectone/imagerecognition/ImagerecognitionApplication.java
@@ -0,0 +1,12 @@
+package com.cloudprojectone.imagerecognition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImagerecognitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImagerecognitionApplication.class, args);
+	}
+}
---
+++ src/test/java/com/cloudprojectone/imagerecognition/ImagerecognitionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cloudprojectone.imagerecognition;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ImagerecognitionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
