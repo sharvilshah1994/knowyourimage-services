eaf920cd0cb21d9f1908c21485c555481546d693
Sharvil Shah <sharvil@sharvil.local>
2018-03-01 21:21:52 -0700

    added upload image

--- src/main/java/com/cloudprojectone/imagerecognition/service/ImageService.java
+++ src/main/java/com/cloudprojectone/imagerecognition/service/ImageService.java
@@ -1,9 +1,10 @@
 package com.cloudprojectone.imagerecognition.service;
 
 import com.cloudprojectone.imagerecognition.model.Image;
+import org.springframework.web.multipart.MultipartFile;
 
 
 public interface ImageService {
 
-    String uploadImage(Image image);
+    String uploadImage(Image image, MultipartFile multipartFile);
 }
--- src/main/java/com/cloudprojectone/imagerecognition/service/implementation/ImageServiceImpl.java
+++ src/main/java/com/cloudprojectone/imagerecognition/service/implementation/ImageServiceImpl.java
@@ -1,12 +1,70 @@
 package com.cloudprojectone.imagerecognition.service.implementation;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.cloudprojectone.imagerecognition.model.Image;
 import com.cloudprojectone.imagerecognition.service.ImageService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.PostConstruct;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+
+@Service
 public class ImageServiceImpl implements ImageService {
 
-    @Override
-    public String uploadImage(Image image) {
-        return null;
+    private AmazonS3 s3client;
+
+    @Value("${amazonProperties.endpointUrl}")
+    private String endpointUrl;
+    @Value("${amazonProperties.bucketName}")
+    private String bucketName;
+    @Value("${amazonProperties.accessKey}")
+    private String accessKey;
+    @Value("${amazonProperties.secretKey}")
+    private String secretKey;
+
+    @PostConstruct
+    private void initializeAmazon() {
+        AWSCredentials credentials = new BasicAWSCredentials(this.accessKey, this.secretKey);
+        AmazonS3 s3client = new AmazonS3Client(credentials);
+    }
+
+    private File convertMultiPartToFile(MultipartFile file) throws IOException {
+        File convFile = new File(file.getOriginalFilename());
+        FileOutputStream fos = new FileOutputStream(convFile);
+        fos.write(file.getBytes());
+        fos.close();
+        return convFile;
+    }
+
+    private String generateFileName(MultipartFile multiPart) {
+        return new Date().getTime() + "-" + multiPart.getOriginalFilename().replace(" ", "_");
+    }
+
+    private void uploadFileTos3bucket(String fileName, File file) {
+        s3client.putObject(new PutObjectRequest(bucketName, fileName, file)
+                .withCannedAcl(CannedAccessControlList.PublicRead));
+    }
+
+    public String uploadImage(Image image, MultipartFile multipartFile) {
+        String fileUrl = "";
+        try {
+            File file = convertMultiPartToFile(multipartFile);
+            String fileName = generateFileName(multipartFile);
+            fileUrl = endpointUrl + "/" + bucketName + "/" + fileName;
+            uploadFileTos3bucket(fileName, file);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return fileUrl;
     }
 }
