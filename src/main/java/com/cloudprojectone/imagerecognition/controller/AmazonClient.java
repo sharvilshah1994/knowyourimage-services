2593269e7436456ad71575a5e39a7e382123bcea
Sharvil Shah <sharvil.shah1994@gmail.com>
2018-03-07 19:50:09 -0700

    fixed as per requirements

--- src/main/java/com/cloudprojectone/imagerecognition/controller/AmazonClient.java
+++ src/main/java/com/cloudprojectone/imagerecognition/controller/AmazonClient.java
@@ -6,15 +6,13 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.CannedAccessControlList;
 import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.cloudprojectone.imagerecognition.model.Image;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.PostConstruct;
+import javax.imageio.ImageIO;
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.Date;
 
 @Service
 public class AmazonClient {
@@ -39,35 +37,26 @@ public class AmazonClient {
         this.s3client = new AmazonS3Client(credentials);
     }
 
-    private File convertMultiPartToFile(MultipartFile file) throws IOException {
-        File convFile = new File(file.getOriginalFilename());
-        FileOutputStream fos = new FileOutputStream(convFile);
-        fos.write(file.getBytes());
-        fos.close();
-        return convFile;
-    }
-
-    private String generateFileName(MultipartFile multiPart) {
-        return new Date().getTime() + "-" + multiPart.getOriginalFilename().replace(" ", "_");
-    }
-
     private void uploadFileTos3bucket(String fileName, File file) {
         s3client.putObject(new PutObjectRequest(bucketName, fileName, file)
                 .withCannedAcl(CannedAccessControlList.PublicRead));
     }
 
-    public String uploadFile(MultipartFile multipartFile) {
+    public void uploadFile(java.awt.image.BufferedImage multipartFile, Image image) {
 
-        String fileUrl = "";
+        String fileUrl;
         try {
-            File file = convertMultiPartToFile(multipartFile);
-            String fileName = generateFileName(multipartFile);
+            String idenImage = image.getIdentifiedImage();
+            idenImage = idenImage.substring(0, idenImage.indexOf("(")).trim();
+            String fileName = image.getId() + "_" + idenImage;
+            File convFile = new File(fileName);
+            ImageIO.write(multipartFile, "PNG", convFile);
             fileUrl = endpointUrl + "/" + bucketName + "/" + fileName;
-            uploadFileTos3bucket(fileName, file);
-            file.delete();
+            image.setUrl(fileUrl);
+            uploadFileTos3bucket(fileName, convFile);
+            convFile.delete();
         } catch (Exception e) {
             e.printStackTrace();
         }
-        return fileUrl;
     }
 }
--- src/main/java/com/cloudprojectone/imagerecognition/controller/ImageController.java
+++ src/main/java/com/cloudprojectone/imagerecognition/controller/ImageController.java
@@ -1,46 +1,60 @@
 package com.cloudprojectone.imagerecognition.controller;
 
 import com.cloudprojectone.imagerecognition.model.Image;
+import com.cloudprojectone.imagerecognition.repository.ImageRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+
+import java.awt.image.BufferedImage;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URL;
 
 @RestController
-@RequestMapping(value = "/image", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(value = "/cloudimagerecognition", produces = MediaType.APPLICATION_JSON_VALUE)
 @CrossOrigin(origins = "*")
 public class ImageController {
 
     private final AmazonClient amazonClient;
 
+    private final ImageRepository imageRepository;
+
     private static final String DIRECTORY = "/home/ubuntu/tensorflow/models/tutorials/image/imagenet/";
 
     @Autowired
-    public ImageController(AmazonClient amazonClient) {
+    public ImageController(AmazonClient amazonClient, ImageRepository imageRepository) {
         this.amazonClient = amazonClient;
+        this.imageRepository = imageRepository;
     }
 
     @RequestMapping(method = RequestMethod.POST)
     @ResponseBody
-    public Image uploadimage(@RequestPart(value = "file", required = false) MultipartFile file) throws IOException {
-        String s3Url = null;
-        String imageRecognized;
-        if (file != null) {
-            s3Url = amazonClient.uploadFile(file);
-        }
+    public Image uploadimage(@RequestParam(value = "input") String imageUrl) throws IOException {
         Image imageAns = new Image();
-        imageAns.setUrl(s3Url);
-        Process p = Runtime.getRuntime().exec("python " + DIRECTORY + "classify_image.py --image_file " + s3Url +
-            " --num_top_predictions 1");
+        imageAns.setUrl(imageUrl);
+        imageAns = imageRepository.save(imageAns);
+        runPythonFile(imageAns);
+        URL url = new URL(imageUrl);
+        BufferedImage bufferedImage = ImageIO.read(url);
+        amazonClient.uploadFile(bufferedImage, imageAns);
+        return imageAns;
+    }
+
+    private void runPythonFile(Image imageAns) throws IOException {
+        String imageRecognized;
+        String pythonCommand = "python " + DIRECTORY + "classify_image.py --image_file " + imageAns.getUrl() +
+                " --num_top_predictions 1";
+        String[] activateTensorFlow = new String[]{"/bin/bash",
+                "-c", "source ~/tensorflow/bin/activate && " + pythonCommand};
+        Process p = Runtime.getRuntime().exec(activateTensorFlow);
         BufferedReader stdInput = new BufferedReader(new
                 InputStreamReader(p.getInputStream()));
         imageRecognized = stdInput.readLine();
         imageAns.setIdentifiedImage(imageRecognized);
-        return imageAns;
     }
 
 }
