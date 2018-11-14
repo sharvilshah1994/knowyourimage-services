14d44675157031a06d7fc235e2fc897bef1878e0
sharvil <sshah-consultant@scholastic.com>
2018-11-13 21:28:13 -0700

    fixes

--- src/main/java/com/cloudprojectone/imagerecognition/ImagerecognitionApplication.java
+++ src/main/java/com/cloudprojectone/imagerecognition/ImagerecognitionApplication.java
@@ -1,13 +1,9 @@
 package com.cloudprojectone.imagerecognition;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@EnableAutoConfiguration
-@ComponentScan(basePackages = "com.cloudprojectone.imagerecognition")
 public class ImagerecognitionApplication {
 
 	public static void main(String[] args) {
